e52b7d3df300c59c9ba2749a980cb7ced227f43c
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-11-16 19:27:23 +0000

    Add files via upload

---
+++ mul.s
@@ -0,0 +1,30 @@
+	.file	"mul.c"
+	.option nopic
+	.attribute arch, "rv64i2p1_m2p0_a2p1_f2p2_d2p2_c2p0_zicsr2p0"
+	.attribute unaligned_access, 0
+	.attribute stack_align, 16
+	.text
+	.align	1
+	.globl	_start
+	.type	_start, @function
+_start:
+	addi	sp,sp,-32
+	sd	s0,24(sp)
+	addi	s0,sp,32
+	li	a5,1
+	sw	a5,-20(s0)
+	li	a5,2
+	sw	a5,-24(s0)
+	sw	zero,-28(s0)
+	lw	a5,-20(s0)
+	mv	a4,a5
+	lw	a5,-24(s0)
+	mulw	a5,a4,a5
+	sw	a5,-28(s0)
+	li	a5,0
+	mv	a0,a5
+	ld	s0,24(sp)
+	addi	sp,sp,32
+	jr	ra
+	.size	_start, .-_start
+	.ident	"GCC: (13.2.0-11ubuntu1+12) 13.2.0"
